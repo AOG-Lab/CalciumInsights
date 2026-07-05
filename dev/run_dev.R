24299a4dff79ce47e33ae8f0ba2dcbc2588e131a
AOG <theappliedoptimizationgroup@gmail.com>
2026-07-05 18:47:03 -0400

    dev

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -1,16 +1,8 @@
-# Set options here
-options(golem.app.prod = FALSE) # TRUE = production mode, FALSE = development mode
+options(golem.app.prod = FALSE)
 
-# Comment this if you don't want the app to be served on a random port
-options(shiny.port = httpuv::randomPort())
+if (!requireNamespace("pkgload", quietly = TRUE)) {
+  stop("Install 'pkgload' before running the development app.", call. = FALSE)
+}
 
-# Detach all loaded packages and clean your environment
-golem::detach_all_attached()
-# rm(list=ls(all.names = TRUE))
-
-# Document and reload your package
-golem::document_and_reload()
-
-# Run the application
+pkgload::load_all(export_all = FALSE, helpers = FALSE, quiet = TRUE)
 run_app()
-
